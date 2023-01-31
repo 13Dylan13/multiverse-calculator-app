4ef03cab91c98549c0fc15884c32655ab5848b50
13Dylan13 <dylan.rickerby@capita.com>
2023-01-31 11:38:42 +0000

    merged the two apps

--- calculator.py
+++ calculator.py
@@ -1,21 +1,14 @@
 import sys
-from calc import add, subtract, multiply, divide
+from calc import add, subtract, multiply, divide, index
+
+def conversions(operation,start_number,operation_number):
+    operation = operation.lower()
+    start_number=float(start_number)
+    operation_number=float(operation_number)
+    calculation(operation,start_number,operation_number)
 
 
-def get_args():
-    try:
-        return (sys.argv[1], int(float(sys.argv[2])), int(float(sys.argv[3])))
-    except IndexError:
-        print("Not all arguments have been provided!")
-        print("USAGE: [operation] [start_number] [operation_number]\n")
-        raise
-    except:
-        print("Unknown error! See the details below.\n")
-        raise
-
-
-def main():
-    operation, start_number, operation_number = get_args()
+def calculation(operation,start_number,operation_number):
     if operation == "add":
         result = add(start_number, operation_number)
     elif operation == "subtract":
@@ -24,11 +17,28 @@ def main():
         result = multiply(start_number, operation_number)
     elif operation == "divide":
         result = divide(start_number, operation_number)
+    elif operation == "index":
+        result = index(start_number, operation_number)
     else:
         raise Exception(f"Unsupported or invalid operation '{operation}'")
-    print(result)
+    print('Answer: ', result)
     return 0
 
+
+def main():
+    print ("")
+    print ("Welcome to the calculator interface v0.2")
+    print ("Enter the function you require, followed be the two numbers")
+    print ("")
+    print ("Available options: ", availableoperation)
+    operation = input('Action: ')
+    start_number = input('Number 1: ')
+    operation_number = input('Number 2: ')
+    print ("")
+    print ("Sum: ", start_number, operation.upper(), operation_number)
+    conversions(operation,start_number,operation_number)
+
+availableoperation = ['Add', 'Subtract', 'Multiply', 'Divide', 'Index']
 
 if __name__ == '__main__':
     sys.exit(main())
